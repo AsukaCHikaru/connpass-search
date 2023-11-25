bd295fbe089f2f76e7929480374639ef765c9dbf
asukachikaru <asukachikaru@gmail.com>
2023-11-25 11:10:44 +0800

    feat: modal base

---
+++ src/components/search/EventModal.tsx
@@ -0,0 +1,34 @@
+import { type Signal, component$ } from "@builder.io/qwik";
+import type { ConnpassEvent } from "~/types/connpass";
+import styles from "../../styles/EventModal.module.css";
+
+interface Props {
+  ref: Signal<HTMLDialogElement | undefined>;
+  event: ConnpassEvent;
+  onClose: () => void;
+}
+
+export default component$<Props>(({ event, ref, onClose }) => {
+  return (
+    <dialog ref={ref} onClick$={onClose}>
+      <div class={styles.modal}>
+        <button class={styles["close-button"]} onClick$={onClose}>
+          X
+        </button>
+        <h2>{event.title}</h2>
+        {event.catch && <h6>{event.catch}</h6>}
+        <p dangerouslySetInnerHTML={event.description}></p>
+        <p>
+          {event.address} {event.place}
+        </p>
+        <p>{event.started_at}</p>
+        <p>
+          {event.accepted}/{event.limit}
+        </p>
+        <a href={event.event_url} target="_blank" rel="noopener noreferrer">
+          link
+        </a>
+      </div>
+    </dialog>
+  );
+});
--- src/routes/search/index.tsx
+++ src/routes/search/index.tsx
@@ -1,10 +1,11 @@
-import { component$ } from "@builder.io/qwik";
+import { $, component$, useSignal, useStore } from "@builder.io/qwik";
 import { routeLoader$ } from "@builder.io/qwik-city";
 import EventCard from "~/components/search/EventCard";
 import KeywordSelector from "~/components/search/KeywordSelector";
 import { fetchEvents } from "~/services/eventService";
-import type { ConnpassEventResponse } from "~/types/connpass";
-import styles from '../../styles/index.module.css';
+import type { ConnpassEvent, ConnpassEventResponse } from "~/types/connpass";
+import styles from "../../styles/index.module.css";
+import EventModal from "~/components/search/EventModal";
 
 export const useEvents = routeLoader$(async (requestEvent) => {
   const keyword = requestEvent.query.get("keyword") || "";
@@ -14,17 +15,43 @@ export const useEvents = routeLoader$(async (requestEvent) => {
 
 export default component$(() => {
   const events = useEvents();
+  const selectedEvent = useStore<{ event: ConnpassEvent | null }>({
+    event: null,
+  });
+  const modalRef = useSignal<HTMLDialogElement>();
+
+  const handleCardClick = $((event: ConnpassEvent) => {
+    selectedEvent.event = event;
+    modalRef.value?.showModal();
+  });
+
+  const handleModalClose = $(() => {
+    modalRef.value?.close();
+  });
 
   return (
-    <div>
-      <div style={{ display: "flex" }}>
-        <KeywordSelector keywords={["javascript", "python", "php", "AI"]} />
+    <>
+      {selectedEvent.event && (
+        <EventModal
+          event={selectedEvent.event}
+          ref={modalRef}
+          onClose={handleModalClose}
+        />
+      )}
+      <div>
+        <div style={{ display: "flex" }}>
+          <KeywordSelector keywords={["javascript", "python", "php", "AI"]} />
+        </div>
+        <div class={styles["card-list"]}>
+          {events.value.events.map((event) => (
+            <EventCard
+              key={event.event_id}
+              event={event}
+              onClick={handleCardClick}
+            />
+          ))}
+        </div>
       </div>
-      <div class={styles['card-list']}>
-        {events.value.events.map((event) => (
-          <EventCard key={event.event_id} event={event} />
-        ))}
-      </div>
-    </div>
+    </>
   );
 });
