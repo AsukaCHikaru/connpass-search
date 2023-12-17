0c513ecf6a6974791094952894df57831b35f80c
asukachikaru <asukachikaru@gmail.com>
2023-12-17 22:33:22 +0900

    feat: prefecture search

--- src/components/search/SearchPreference.tsx
+++ src/components/search/SearchPreference.tsx
@@ -1,11 +1,13 @@
 import { component$ } from "@builder.io/qwik";
 import KeywordSelector from "./KeywordSelector";
 import styles from './SearchPreference.module.css';
+import { KEYWORDS, PREFECTURES } from "~/consts/requestOrder";
 
 export default component$(() => {
   return (
     <div class={styles['preference-wrapper']}>
-      <KeywordSelector keywords={["javascript", "python", "php", "AI"]} />
+      <KeywordSelector keywords={KEYWORDS} />
+      <KeywordSelector keywords={PREFECTURES} />
     </div>
   );
 });
