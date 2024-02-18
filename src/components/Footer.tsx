ea0aa79ff16bd235ebeefaaf1d12c97aa9bb73b9
asukachikaru <asukachikaru@gmail.com>
2024-02-18 15:53:58 +0900

    feat: add github link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,6 +7,13 @@ export const Footer = component$(() => {
       <p class={styles["footer-disclaimer"]}>
         {`connpass search is an independent website and is not associated with BeProud or copnnpass.\nconnpass is registered trademarks of BeProud Inc..`}
       </p>
+      <a
+        href="https://github.com/AsukaCHikaru/connpass-search"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        GitHub
+      </a>
     </footer>
   );
 });
